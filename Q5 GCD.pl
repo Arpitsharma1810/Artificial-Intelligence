1d4241e967d5cabb5b1162d0f302011a4e1f8c17
Arpitsharma1810 <89004667+Arpitsharma1810@users.noreply.github.com>
2023-05-03 01:29:23 +0530

    Update Q5 GCD.pl

--- Q5 GCD.pl
+++ Q5 GCD.pl
@@ -4,20 +4,9 @@ GCD of two numbers
 gcd(+X, +Y, -D)
     D is gcd(X,Y).
 */
-gcd(0, 0, 0) :- !. % '!' is called cut & used for stopping more recursion & move process backwards. 
-gcd(0, Y, Y) :- !.
-gcd(X, 0, X) :- !.
-gcd(X, Y, D) :- 
-    X < 0, 
-    gcd(-X, Y, D).
-gcd(X, Y, D) :- 
-    Y < 0, 
-    gcd(X, -Y, D).
-gcd(X, Y, D) :- 
-    X > Y, 
-    Z is mod(X, Y),
-    gcd(Z, Y, D).
-gcd(X, Y, D) :- 
-    X =< Y, 
-    Z is mod(Y, X),
-    gcd(X, Z, D).
+gcd(X,X,X).
+gcd(X,Y,D):-X<Y,
+    Y1 is Y-X,
+    gcd(X,Y1,D).
+    gcd(X,Y,D):-Y<X,
+    gcd(Y,X,D).
